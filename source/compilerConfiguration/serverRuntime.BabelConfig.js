7f8b133cd5318b3e718c441df0a9ca59b83c12f5
Dr. Safi <sfmissive@gmail.com>
2018-08-18 18:25:02 +0300

    Update Babel configuration to compile internal node_modules.

--- source/compilerConfiguration/serverRuntime.BabelConfig.js
+++ source/compilerConfiguration/serverRuntime.BabelConfig.js
@@ -23,7 +23,7 @@ let plugins = {
 
 module.exports = Object.assign({
     // When a file path matches this regex then it is **not** compiled
-    "ignore": [/node_modules\/(?!appscript)/], // ignore everythng in node_modules except internal modules.
+    "ignore": [/node_modules\/(?!@dependency)/], // ignore everythng in node_modules except internal modules.
     // Setting this will remove the currently hooked extensions of `.es6`, `.es`, `.jsx`, `.mjs`
     // and .js so you'll have to add them back if you want them to be used again.
     extensions: [".js", ".mjs"], // default is ".es6", ".es", ".jsx", ".js", ".mjs"
