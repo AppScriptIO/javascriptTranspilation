84ec8c53632e7712286c90338fb9f34d07510637
Safi <sfmissive@gmail.com>
2019-03-13 10:37:36 +0200

    ➕ Optional chaining operator plugin.

--- source/compilerConfiguration/serverRuntime.BabelConfig.js
+++ source/compilerConfiguration/serverRuntime.BabelConfig.js
@@ -23,6 +23,7 @@ module.exports = {
       require.resolve(`babel-plugin-transform-function-parameter-decorators`), // function parameter decorator
       require.resolve(`@babel/plugin-proposal-function-bind`), // Bind shorthand replaces `.call` & `.bind`
       require.resolve(`@babel/plugin-proposal-function-sent`), // Adds `function.sent` meta propety in generators
+      require.resolve(`@babel/plugin-proposal-optional-chaining`), // Optional chaining operator e.g. `obj.key1?.key2?.key3`
     ]
   },
 
